cf599858e38df535fdafe56316d59a690f619303
Jason Jin <jgjin@umich.edu>
2025-10-13 13:07:10 -0400

    caches album list per access token with TTL

--- app/sections/album/routes/__init__.py
+++ app/sections/album/routes/__init__.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+import time
+
 from fastapi import APIRouter, Request, HTTPException
 from spotipy import Spotify
 
@@ -11,17 +14,30 @@ async def list(request: Request) -> ListAlbumResponse:
     access_token = request.session.get("access_token")
     if not access_token:
         raise HTTPException(status_code=401)
+
+    albums = [
+        Album(
+            image_url=item["album"]["images"][0]["url"],
+            name=item["album"]["name"],
+            artist=item["album"]["artists"][0]["name"],
+        )
+        for item in list_albums(access_token, get_ttl_hash(ttl_seconds=60))
+    ]
+
+    return ListAlbumResponse(albums=albums)
+
+
+@lru_cache()
+def list_albums(access_token: str, ttl_hash: int) -> list[dict]:
     spotify = Spotify(auth=access_token)
+    del ttl_hash
 
     albums = []
     while chunk := spotify.current_user_saved_albums(offset=len(albums))["items"]:
-        albums.extend(
-            Album(
-                image_url=item["album"]["images"][0]["url"],
-                name=item["album"]["name"],
-                artist=item["album"]["artists"][0]["name"],
-            )
-            for item in chunk
-        )
+        albums.extend(chunk)
 
-    return ListAlbumResponse(albums=albums)
+    return albums
+
+
+def get_ttl_hash(ttl_seconds: int):
+    return round(time.time() / ttl_seconds)
