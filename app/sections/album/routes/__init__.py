0af04954bf6f4d32b55edd11cbda8c8f827e0707
Jason Jin <jgjin@umich.edu>
2025-10-19 01:29:14 -0400

    replaces spotipy with async aiohttp

--- app/sections/album/routes/__init__.py
+++ app/sections/album/routes/__init__.py
@@ -1,10 +1,10 @@
-from functools import lru_cache
 from random import shuffle
 import time
 from typing import Sequence
 
+import aiohttp
+from async_lru import alru_cache
 from fastapi import APIRouter, Request, HTTPException
-from spotipy import Spotify
 
 from app.sections.album.models import Album, ListAlbumResponse
 
@@ -24,23 +24,44 @@ async def list(request: Request) -> ListAlbumResponse:
             artist=item["album"]["artists"][0]["name"],
             spotify_url=item["album"]["external_urls"]["spotify"],
         )
-        for item in list_albums(access_token, get_ttl_hash(ttl_seconds=(12 * 60 * 60)))
+        for item in (
+            await list_albums(access_token, get_ttl_hash(ttl_seconds=(24 * 60 * 60)))
+        )
     ]
     shuffle(albums)
 
     return ListAlbumResponse(albums=albums)
 
 
-@lru_cache()
-def list_albums(access_token: str, ttl_hash: int) -> Sequence[dict]:
-    spotify = Spotify(auth=access_token)
+@alru_cache()
+async def list_albums(access_token: str, ttl_hash: int) -> Sequence[dict]:
     del ttl_hash
 
     albums = []
-    while chunk := spotify.current_user_saved_albums(offset=len(albums))["items"]:
-        albums.extend(chunk)
+    async with aiohttp.ClientSession(
+        headers={"Authorization": f"Bearer {access_token}"}
+    ) as session:
+        while chunk := (await get_saved_albums(session, limit=20, offset=len(albums)))[
+            "items"
+        ]:
+            albums.extend(chunk)
 
     return albums
+
+
+async def get_saved_albums(
+    session: aiohttp.ClientSession,
+    limit: int,
+    offset: int,
+) -> Sequence[dict]:
+    async with session.get(
+        "https://api.spotify.com/v1/me/albums",
+        params={
+            "limit": limit,
+            "offset": offset,
+        },
+    ) as response:
+        return await response.json()
 
 
 def get_ttl_hash(ttl_seconds: int) -> int:
